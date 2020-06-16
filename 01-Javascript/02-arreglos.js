7981132db66dc0a16fbe93a87c3732069977d4da
Vinicio Quinga <vinicio_520@hotmail.com>
2020-06-16 17:59:56 -0500

    Variables y arreglos

---
+++ 01-Javascript/02-arreglos.js
@@ -0,0 +1,15 @@
+const arreglo = [1,2,3,4,5,6,7,8,9,10];
+arreglo = 1;
+
+let cualquierCosa = "ASD";
+cualquierCosa = 1;
+cualquierCosa = true;
+cualquierCosa = undefined;
+cualquierCosa = null;
+cualquierCosa = {};
+cualquierCosa = [];
+
+const arregloTodo = [ true, 1, 1.2, "Adrian", undefined, null, {}, [1,2, true , "A"]];
+
+let a = [1,2,3];
+
