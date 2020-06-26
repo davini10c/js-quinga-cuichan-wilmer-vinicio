763df373e56147e3b261452f72178936d754befa
Vinicio Quinga <vinicio_520@hotmail.com>
2020-06-26 18:06:20 -0500

    Callbacks y destructurar

--- 01-Javascript/04-funciones.js
+++ 01-Javascript/04-funciones.js
@@ -55,3 +55,5 @@ return numeroInicial + otrosNumeros.reduce((a, v)=> a+v,0);
 
 }
 sumarNumeros(1, 2,3,4,5,6,7,8,9,10);
+
+
---
+++ 01-Javascript/06-callbacks.js
@@ -0,0 +1,23 @@
+
+const fs = require('fs');
+
+console.log('Primero'); //sincrona
+
+// callback es una funcion que se ejecuta dentro de las funciones asincronas y devulve un resultado
+fs.readFile( //asincrona
+    './07-ejemplo.txt',
+    'utf-8',
+    (error, contenido )=>{
+        console.log('Segundo');
+        if (error){
+            console.log('Hubo error', error);
+        }
+        else
+        {
+            console.log(contenido);
+        }
+    }
+);
+const suma = 5+3; // sincrona
+console.log('suma', suma); //sincrona
+console.log('Final'); //sincrona
